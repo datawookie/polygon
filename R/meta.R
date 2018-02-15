4be83292dca3b78ade181cd594c3d687b4bb6047
Andrew B. Collier <andrew@exegetic.biz>
2018-02-15 08:17:12 +0200

    Started documentation.

--- R/meta.R
+++ R/meta.R
@@ -1,3 +1,9 @@
+#' Title
+#'
+#' @return
+#' @export
+#'
+#' @examples
 meta_symbols <- function() {
   url = create_url("v1/meta/symbols", list(sort = "symbol", perpage = 50, page = 1))
   GET(url) %>% content("text") %>% fromJSON() %>% .$symbols
