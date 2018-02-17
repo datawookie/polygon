3283aed3ad13b706535325341b159ffafc75f5d3
Andrew B. Collier <andrew@exegetic.biz>
2018-02-17 03:30:07 +0200

    Check for API key.

--- R/authenticate.R
+++ R/authenticate.R
@@ -1,5 +1,3 @@
-polygon_env <- new.env()
-
 #' Title
 #'
 #' @param api_key
@@ -13,4 +11,13 @@ set_api_key <- function(api_key) {
     stop("Invalid API key!", call. = FALSE)
   }
   polygon_env$api_key <- api_key
+}
+
+check_api_key <- function() {
+  if (is.null(polygon_env$api_key)) {
+    stop("No API key has been set. Please call set_api_key()!", call. = FALSE)
+    FALSE
+  } else {
+    TRUE
+  }
 }
--- R/last-quote.R
+++ R/last-quote.R
@@ -8,10 +8,13 @@
 #' @examples
 #' last_quote_forex("AUD/USD")
 last_quote_forex <- function(pair) {
+  check_api_key()
+  #
   from_to = strsplit(pair, "/")[[1]]
   #
   url = paste("v1/last_quote/currencies", from_to[1], from_to[2], sep = "/")
   url = create_url(url)
+  print(url)
   #
   result = GET(url) %>% content("text") %>% fromJSON()
   #
