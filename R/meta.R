7b6576fd10191f10862635371aa1a6d5fcac5f62
Andrew B. Collier <andrew@exegetic.biz>
2018-03-01 15:21:08 +0200

    Check for API key.

--- R/meta.R
+++ R/meta.R
@@ -3,6 +3,8 @@
 #' @return A data frame
 #' @export
 meta_symbols <- function() {
+  check_api_key()
+  #
   url = create_url("v1/meta/symbols", list(sort = "symbol", perpage = 50, page = 1))
   GET(url) %>% content("text") %>% fromJSON() %>% .$symbols
 }
@@ -12,6 +14,8 @@ meta_symbols <- function() {
 #' @return A vector
 #' @export
 meta_currency_symbols <- function() {
+  check_api_key()
+  #
   url = create_url("v1/meta/currency-symbols", list())
   GET(url) %>% content("text") %>% fromJSON()
 }
