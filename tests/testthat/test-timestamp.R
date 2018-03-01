b4cf8d09ab3758af9e776420a366902dadd8e100
Andrew B. Collier <andrew@exegetic.biz>
2018-03-01 15:21:20 +0200

    Use tz for as.POSIXct().

--- tests/testthat/test-timestamp.R
+++ tests/testthat/test-timestamp.R
@@ -1,7 +1,7 @@
 context("timestamp")
 
 test_that("convert timestamp to POSIXct", {
-  expect_equal(timestamp_to_posix(1517529605225), as.POSIXct("2018-02-02 00:00:05.225"))
+  expect_equal(timestamp_to_posix(1517529605225), as.POSIXct("2018-02-02 00:00:05.225", tz = "UTC"))
 })
 
 test_that("convert POSIXct to timestamp", {
